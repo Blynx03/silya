4e1f39184ecfd28f47b8c53b6dfafc1b0abbd184
Charlie Chan <cchan_03@yahoo.ca>
2023-11-20 19:05:15 -0500

    update build for gh-pages

--- src/components/SwitchNavHeader.js
+++ src/components/SwitchNavHeader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import Gallery from "../pages/Gallery";
 import Contact from "../pages/Contact";
@@ -11,14 +11,14 @@ import Payment from "../pages/Payment";
 const SwitchNavHeader = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigate to="/home" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/gallery" element={<Gallery />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/details" element={<Details />} />
       <Route path="/checkout" element={<Checkout />} />
       <Route path="/payment" element={<Payment />} />
-      <Route index element={<Home />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   );
 };
